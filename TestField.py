a60a241060c72d1ddb923466fbd3a6682f6bce6b
Luca A <luca@rosrunner.com>
2021-12-02 18:55:44 +0100

    removing AbstractAdapter

--- TestField.py
+++ TestField.py
@@ -1,4 +1,5 @@
 import signal
+import sys
 from threading import Thread
 from time import sleep
 
@@ -32,7 +33,10 @@ if __name__ == "__main__":
         quit(1)
 
     try:
-        server = VirtualCapabilityServer()
+        port = None
+        if len(sys.argv[1:]) > 0:
+            port = int(sys.argv[1])
+        server = VirtualCapabilityServer(port)
         listener = TestField(server)
         listener.start()
         signal.signal(signal.SIGTERM, handler)
@@ -40,4 +44,6 @@ if __name__ == "__main__":
         # Needed for properly closing, when program is being stopped wit a Keyboard Interrupt
     except KeyboardInterrupt:
         print("[Main] Received KeyboardInterrupt")
+        server.kill()
         listener.kill()
+
