c5b02982d8d23b79dab104af67eb3bfaf1a4fe21
Luca Alfano(HIWI) <luca.alfano@isse.de>
2022-12-07 14:48:15 +0100

    + small bugfix

--- TestField.py
+++ TestField.py
@@ -11,7 +11,7 @@ class TestField(AbstractVirtualCapability):
 
     def __init__(self, server):
         super().__init__(server)
-        self.TestFieldBoundaries = [[2, -2, 0.], [-2., 2., 0.]]
+        self.TestFieldBoundaries = [[1, -1, 0.], [-1., 1., 0.]]
         '''
         try:
             self.TestFieldBoundaries = pickle.loads("TestFieldBoundaries")
@@ -36,25 +36,17 @@ class TestField(AbstractVirtualCapability):
 
 
 if __name__ == "__main__":
-    # Needed for properly closing when process is being stopped with SIGTERM signal
-    def handler(signum, frame):
-        print("[Main] Received SIGTERM signal")
-        listener.kill()
-        quit(1)
-
     try:
         port = None
         if len(sys.argv[1:]) > 0:
             port = int(sys.argv[1])
         server = VirtualCapabilityServer(port)
-        listener = TestField(server)
-        listener.start()
-        signal.signal(signal.SIGTERM, handler)
-        listener.join()
+        tf = TestField(server)
+        tf.start()
+        while server.running:
+            pass
         # Needed for properly closing, when program is being stopped wit a Keyboard Interrupt
     except KeyboardInterrupt:
         print("[Main] Received KeyboardInterrupt")
-        server.kill()
-        listener.kill()
 
 
